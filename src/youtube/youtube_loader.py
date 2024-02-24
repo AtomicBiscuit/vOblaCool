bb0097b5efbb97531d7f2186896f2266f5fc16fd
AtomicBiscuit <152898934+atomicbiscuit@users.noreply.github.com>
2024-02-24 19:30:32 +0300

    Add temporary version of youtube loader

---
+++ src/youtube/youtube_loader.py
@@ -0,0 +1,47 @@
+from http import HTTPStatus
+
+import flask
+from decouple import config
+from flask import request, Response
+from pytube import YouTube
+from pytube.exceptions import AgeRestrictedError, VideoPrivate, PytubeError
+
+
+# Временное решение
+class YLoaderPrototype:
+    def __init__(self):
+        self.app = flask.Flask(__name__)
+        self.host = config('YOUTUBE_LOADER_HOST')
+        self.port = config('YOUTUBE_LOADER_PORT')
+        self.__configure_router()
+
+    @staticmethod
+    async def __main_page() -> Response:
+        return Response(f'Ok', HTTPStatus.OK)
+
+    @staticmethod
+    async def __download() -> Response:
+        payload = request.json
+        url_raw = payload['url']
+        code = HTTPStatus.OK
+        file_path = None
+        try:
+            videos = YouTube(url_raw).streams.filter(progressive=True, file_extension='mp4', resolution='720p').desc()
+            file_path = videos.first().download('../media')
+        except (AgeRestrictedError, VideoPrivate) as e:
+            code = HTTPStatus.UNAUTHORIZED
+        except PytubeError as e:
+            code = HTTPStatus.BAD_REQUEST
+        return Response(file_path, status=code)
+
+    def __configure_router(self):
+        self.app.add_url_rule('/', view_func=self.__main_page, methods=['GET'])
+        self.app.add_url_rule('/api/download', view_func=self.__download, methods=['POST'])
+
+    def run(self, debug: bool = True) -> None:
+        self.app.run(debug=debug, host=self.host, port=self.port, use_reloader=False)
+
+
+if __name__ == '__main__':
+    app = YLoaderPrototype()
+    app.run()
