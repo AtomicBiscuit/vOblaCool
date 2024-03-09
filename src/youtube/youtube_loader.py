1afa53de3fbc53f0b577fd28588f627cf17e1e03
Alexander Vasyukov <avvasiukov@edu.hse.ru>
2024-03-09 22:02:13 +0300

    logger init

--- src/youtube/youtube_loader.py
+++ src/youtube/youtube_loader.py
@@ -6,8 +6,15 @@ from flask import request, Response
 from pytube import YouTube
 from pytube.exceptions import AgeRestrictedError, VideoPrivate, PytubeError
 
+import datetime
+import json
 import logging
 import os
+import ssl
+
+app_env = config("APP_ENVIRONMENT")
+if app_env:
+    ssl._create_default_https_context = ssl._create_stdlib_context
 
 logger = logging.getLogger("YOUTUBE_LOADER")
 
@@ -21,8 +28,18 @@ logger.addHandler(handler)
 
 sep = os.sep
 
-# Временное решение
-class YLoaderPrototype:
+class YoutubeLoader:
+    """
+    Класс для загрузки видео и получения информация о плейлистах Youtube
+
+    :param app: Flask-приложение для общения с другими модулями
+    :type app: :class: `flask.app.Flask`
+    :param host: Хост для запуска
+    :type host: :class: `str`
+    :param port: Порт для запуска
+    :type port: :class: `int`
+    """
+
     def __init__(self):
         self.app = flask.Flask(__name__)
         self.host = config('YOUTUBE_LOADER_HOST')
@@ -31,32 +48,55 @@ class YLoaderPrototype:
 
     @staticmethod
     async def main_page() -> Response:
+        """
+        Обрабатывает GET-запросы на главную страницу
+
+        :return: Response 200
+        """
         return Response(f'Ok', HTTPStatus.OK)
 
     @staticmethod
     async def download() -> Response:
+        """
+        Загружает видео с использованием библиотеки pytube
+
+        :return: Response 200 с путём к файлу если загрузка удалась, BadResponse 413|400 иначе
+        """
         payload = request.json
         url_raw = payload['url']
         code = HTTPStatus.OK
         file_path = None
         try:
+            logger.info(f'Download start url: {url_raw}')
             videos = YouTube(url_raw).streams.filter(progressive=True, file_extension='mp4', resolution='720p').desc()
             file_path = videos.first().download('../media')
+            logger.info(f'Download complete, file: {file_path}')
         except (AgeRestrictedError, VideoPrivate) as e:
+            logger.warning(f"Cath: {e.__class__.__name__}, {e}, {e.args}")
             code = HTTPStatus.UNAUTHORIZED
         except PytubeError as e:
+            logger.error(f"Cath unexpected error: {e.__class__.__name__}, {e}, {e.args}")
             code = HTTPStatus.BAD_REQUEST
         return Response(file_path, status=code)
 
     def configure_router(self):
+        """
+        Прописывает все пути для взаимодействия с Flask
+        """
         self.app.add_url_rule('/', view_func=self.main_page, methods=['GET'])
         self.app.add_url_rule('/api/download', view_func=self.download, methods=['POST'])
+        # self.app.add_url_rule('/api/get/playlist', view_func=self.get_playlist, methods=['GET'])
 
     def run(self, debug: bool = True) -> None:
+        """
+        Запускает приложение
+
+        :param debug: Запуск приложения в debug режиме
+        """
         logger.info('Starting')
         self.app.run(debug=debug, host=self.host, port=self.port, use_reloader=False)
 
 
 if __name__ == '__main__':
-    app = YLoaderPrototype()
+    app = YoutubeLoader()
     app.run()
